c0914c683a95d24552cbcc74602e6c4c0f643f55
blueshack112 <ahmed.hassan.112.ha@gmail.com>
2019-04-16 22:44:17 +0500

    Finishing touches

--- app/src/main/java/com/hassan/android/fyp_app_final/RoomSelectionSpinnerAdapter.java
+++ app/src/main/java/com/hassan/android/fyp_app_final/RoomSelectionSpinnerAdapter.java
@@ -3,6 +3,7 @@ package com.hassan.android.fyp_app_final;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -18,14 +19,13 @@ public class RoomSelectionSpinnerAdapter extends ArrayAdapter<String> {
         super(context, resource, context.getResources().getStringArray(R.array.Rooms));
         this.context = context;
         this.options = context.getResources().getStringArray(R.array.Rooms);
-        defaultOption = context.getResources().getString(R.string.default_text_room_spinner);
-        isFirstTime = true;
-        setDefaultOption();
-    }
-    private void setDefaultOption () {
         firstOption = options[0];
         options[0] = defaultOption;
+        defaultOption = context.getResources().getString(R.string.default_text_room_spinner);
+        isFirstTime = true;
     }
+
+
 
     @Override
     public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
@@ -33,6 +33,8 @@ public class RoomSelectionSpinnerAdapter extends ArrayAdapter<String> {
             options[0] = firstOption;
             isFirstTime = false;
         }
+        Log.d("imp", options[0]);
+
         return super.getDropDownView(position, convertView, parent);
     }
 }
