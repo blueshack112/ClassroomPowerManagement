4f34de501f0178eff6b8c7f44b22c48244d7a66e
blueshack112 <ahmed.hassan.112.ha@gmail.com>
2019-04-21 17:21:12 +0500

    Logout button completed.

--- app/src/main/java/com/hassan/android/fyp_app_final/UserHome.java
+++ app/src/main/java/com/hassan/android/fyp_app_final/UserHome.java
@@ -1,5 +1,7 @@
 package com.hassan.android.fyp_app_final;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.content.Intent;
@@ -9,6 +11,7 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutCompat;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 public class UserHome extends AppCompatActivity {
@@ -40,5 +43,32 @@ public class UserHome extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.user_action_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.logout) {
+            onLogOutClicked();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    public void onLogOutClicked () {
+        AlertDialog.Builder logoutAlert = new AlertDialog.Builder(this);
+        logoutAlert.setTitle("Confirm Logout");
+        logoutAlert.setMessage("Are you sure you want to logout?");
+        logoutAlert.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent mainActivityIntent = new Intent(UserHome.this, MainActivity.class);
+                startActivity(mainActivityIntent);
+            }
+        });
+        logoutAlert.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(UserHome.this, "you chose to not logout.", Toast.LENGTH_SHORT).show();
+            }
+        });
+        logoutAlert.show();
     }
 }
